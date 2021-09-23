bc492910827091ecf3fe1bfa29356b52dddee3e3
Donish <nurakh99@gmail.com>
2021-09-23 12:55:32 +0500

    cors problem

--- src/modules/portfolio/pages/SearchCoinsPage.tsx
+++ src/modules/portfolio/pages/SearchCoinsPage.tsx
@@ -1,18 +1,20 @@
-import { useEffect } from 'react';
 import Container from 'Components/Container';
 import Navbar from 'Components/Navbar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { getCoinsList } from 'services/coinMarketCap.service';
 
 const Search = () => {
-    useEffect(async () => {
-        console.log(await getCoinsList());
+  useEffect(() => {
+    getCoinsList().then((data) => {
+      console.log(data);
     });
-    return (
-        <Container>
-            <Navbar />
-        </Container>
-    );
+  });
+
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
 };
 
 export default Search;
